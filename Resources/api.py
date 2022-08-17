4cf1c239506f909e97f18236f613876695987a59
zhangshuai <zhangshuai@cnipr.com>
2022-08-17 15:03:22 +0800

    20220817 15:00提交

---
+++ Resources/api.py
@@ -0,0 +1,174 @@
+# -*- coding:utf-8 -*-
+# /usr/bin/env python
+"""
+Date: 2022/2/22 16:05
+Desc: HTTP 模式主文件
+"""
+import json
+import urllib.parse
+
+import akshare as ak
+from fastapi import APIRouter
+from fastapi import Depends, status
+from fastapi import Request, Response
+from fastapi.responses import JSONResponse, HTMLResponse
+
+from aktools.login.user_login import User, get_current_active_user
+from aktools.datasets import get_pyscript_html
+
+app_core = APIRouter()
+
+
+@app_core.get("/private/{item_id}", description="私人接口", summary="该接口主要提供私密访问来获取数据")
+def root(
+    request: Request,
+    item_id: str,
+    current_user: User = Depends(get_current_active_user),
+):
+    """
+    接收请求参数及接口名称并返回 JSON 数据
+    此处由于 AKShare 的请求中是同步模式，所以这边在定义 root 函数中没有使用 asyncio 来定义，这样可以开启多线程访问
+    :param request: 请求信息
+    :type request: Request
+    :param item_id: 必选参数; 测试接口名 stock_dxsyl_em 来获取 打新收益率 数据
+    :type item_id: str
+    :param current_user: 依赖注入，为了进行用户的登录验证
+    :type current_user: str
+    :return: 指定 接口名称 和 参数 的数据
+    :rtype: json
+    """
+    interface_list = dir(ak)
+    decode_params = urllib.parse.unquote(str(request.query_params))
+    # print(decode_params)
+    if item_id not in interface_list:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "error": "未找到该接口，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+            },
+        )
+    eval_str = decode_params.replace("&", '", ').replace("=", '="') + '"'
+    if not bool(request.query_params):
+        try:
+            received_df = eval("ak." + item_id + f"()")
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+    else:
+        try:
+            received_df = eval("ak." + item_id + f"({eval_str})")
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+
+
+@app_core.get("/public/{item_id}", description="公开接口", summary="该接口主要提供公开访问来获取数据")
+def root(request: Request, item_id: str):
+    """
+    接收请求参数及接口名称并返回 JSON 数据
+    此处由于 AKShare 的请求中是同步模式，所以这边在定义 root 函数中没有使用 asyncio 来定义，这样可以开启多线程访问
+    :param request: 请求信息
+    :type request: Request
+    :param item_id: 必选参数; 测试接口名 stock_dxsyl_em 来获取 打新收益率 数据
+    :type item_id: str
+    :return: 指定 接口名称 和 参数 的数据
+    :rtype: json
+    """
+    interface_list = dir(ak)    
+    decode_params = urllib.parse.unquote(str(request.query_params))
+    #print(decode_params)   
+    if item_id not in interface_list:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "error": "未找到该接口，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+            },
+        )
+    eval_str = decode_params.replace('&', ';')#还原;
+    #eval_str = eval_str.replace(' ', '')#去掉参数内的空格
+    #print(eval_str)    
+    eval_str = eval_str.replace("=", '="', 1) + '"'#给第一个等号加上"
+    #print(eval_str)    
+    eval_str = eval_str.replace("+", " ")  # 处理传递的参数中带空格的情况
+    #print(eval_str)    
+    if not bool(request.query_params):
+        try:
+            received_df = eval("ak." + item_id + f"()")
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+    else:
+        try:
+            cmd="ak." + item_id + f"({eval_str})"
+            #print(cmd)            
+            received_df = eval(cmd)
+            if received_df is None:
+                return JSONResponse(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    content={"error": "该接口返回数据为空，请确认参数是否正确：https://www.akshare.xyz"},
+                )
+            temp_df = received_df.to_json(orient="records", date_format="iso")
+        except KeyError as e:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={
+                    "error": f"请输入正确的参数错误 {e}，请升级 AKShare 到最新版本并在文档中确认该接口的使用方式：https://www.akshare.xyz"
+                },
+            )
+        return JSONResponse(status_code=status.HTTP_200_OK, content=json.loads(temp_df))
+
+
+def generate_html_response():
+    file_path = get_pyscript_html(file="akscript.html")
+    with open(file_path, encoding="utf8") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app_core.get(
+    "/show-temp/{interface}",
+    response_class=HTMLResponse,
+    description="展示 PyScript",
+    summary="该接口主要展示 PyScript 游览器运行 Python 代码",
+)
+
+@app_core.get(
+    "/show",
+    response_class=HTMLResponse,
+    description="展示 PyScript",
+    summary="该接口主要展示 PyScript 游览器运行 Python 代码",
+)
+def akscript():
+    return generate_html_response()
